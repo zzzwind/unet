8ef76e4652e2e177a352a38f28958dda739106af
kunkun <zykblack@gmail.com>
2023-03-03 22:16:22 +0800

    添加了计算指标，修改了一些代码，添加了tensorboard支持

--- metrics.py
+++ metrics.py
@@ -1,17 +1,56 @@
-import torchmetrics
+import torch
 
-def f1_score():
-    pass
+def _confusion_matrix(pred, label, class_id):
+    TP = ((pred == class_id) & (label == class_id)).sum().item()
+    FP = ((pred == class_id) & (label != class_id)).sum().item()
+    TN = ((pred != class_id) & (label != class_id)).sum().item()
+    FN = ((pred != class_id) & (label == class_id)).sum().item()
+    return TP, FP, TN, FN
+
+def get_confusion_matrix(pred, label, num_classes):
+    pred = torch.argmax(pred, dim=1)
+    cm = torch.zeros((num_classes, num_classes), dtype=torch.int)
+    for i in range(num_classes):
+        TP, FP, TN, FN = _confusion_matrix(pred, label, i)
+        cm[i][i] = TP
+        cm[i][0:i] = FN / (num_classes - 1)
+        cm[i][i + 1:] = FP / (num_classes - 1)
+    return cm
+
+def get_pixel_accuracy(pred, label, num_classes):
+    confusion_matrix = get_confusion_matrix(pred, label, num_classes)
+    return torch.diag(confusion_matrix).sum() / confusion_matrix.sum()
 
 
-def get_miou():
-    pass
+def recall(pred, label, num_classes):
+    # confusion_matrix: shape=(num_classes, num_classes)
+    # return: recall
+    confusion_matrix = get_confusion_matrix(pred, label, num_classes)
+    return torch.diag(confusion_matrix) / (confusion_matrix.sum(dim=1) + 1e-20)
 
+def f1_score(pred, label, num_classes):
+    # confusion_matrix: shape=(num_classes, num_classes)
+    # return: F1-score
+    confusion_matrix = get_confusion_matrix(pred, label, num_classes)
+    precision = torch.diag(confusion_matrix) / (confusion_matrix.sum(dim=0) + 1e-20)
+    recall = torch.diag(confusion_matrix) / (confusion_matrix.sum(dim=1) + 1e-20)
+    return 2 * (precision * recall) / (precision + recall + 1e-20)
 
-def get_pixel_acc():
-    pass
+def miou(pred, label, num_classes):
+    # confusion_matrix: shape=(num_classes, num_classes)
+    # return: mIoU
+    confusion_matrix = get_confusion_matrix(pred, label, num_classes)
+    intersection = torch.diag(confusion_matrix)
+    union = confusion_matrix.sum(dim=0) + confusion_matrix.sum(dim=1) - intersection
+    iou = intersection / (union + 1e-20)
+    return iou.mean()
 
-
-
-if __name__ == '__main__ ':
-    pass
+if __name__ == "__main__":
+    pred = torch.randn(2, 2, 256, 256)
+    label = torch.randn(2, 256, 256)
+    cm = get_confusion_matrix(pred, label, 2)
+    pa = get_pixel_accuracy(cm)
+    f1 = f1_score(cm)
+    recall = recall(cm)
+    miou = miou(cm)
+    print(cm)
--- train.py
+++ train.py
@@ -5,10 +5,13 @@ import torch
 from utils.loss_fun import *
 from tqdm import tqdm
 from utils.my_metircs import confusion_matrix
+import os
+from metrics import get_pixel_accuracy
+from torch.utils.tensorboard import SummaryWriter
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-weight_path = 'weight/change-net/change-net.pth'
-img_path = 'images'
+weight_path = './param/change-net.pth'
+img_path = r'F:\dataset\whu-change-detection\images'
 
 full_ds = ChangeNetDataset(path=img_path)
 split_rate = 0.8
@@ -19,51 +22,63 @@ val_size = len(full_ds) - train_size
 train_ds, val_ds = torch.utils.data.random_split(full_ds, [train_size, val_size])
 train_dl = DataLoader(
     dataset=train_ds,
-    batch_size=2,
+    batch_size=32,
     shuffle=True,
-    # num_workers=4,
+    # num_workers=os.cpu_count(),
 )
 val_dl = DataLoader(
     dataset=val_ds,
-    batch_size=4,
-    shuffle=True,
-    # num_workers=4,
+    batch_size=64,
+    shuffle=False,
+    # num_workers=os.cpu_count(),
 )
 
 model = ChangeUnet(in_channels=3, out_channels=2, init_features=16)
-
+model.to(device)
 optimizer = torch.optim.Adam(model.parameters())
 loss_fn =nn.CrossEntropyLoss()
 # loss_fun = torchmetrics.Dice()
 
+writer = SummaryWriter(log_dir='./log')
 
 epochs = 50
-for i in range(epochs):
+for epoch in range(epochs):
     total_loss = 0
     total_acc = 0
     pbar = tqdm(enumerate(train_dl), total=len(train_dl), desc="epoch")
     model.train()
     for index, (before_image, after_image, label_image) in pbar:
-
+        before_image, after_image, label_image = before_image.to(device), after_image.to(device), label_image.to(device)
         output = model(before_image, after_image)
         # loss = 1 - loss_fun(output, label_image.int())
         # loss.requires_grad_(True)
         loss = loss_fn(output, label_image.long())
-        total_loss += loss.item()
+        total_loss += loss.item() * output.size(0)
+
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        #acc
-        _pred, _y = torch.max(output, dim=1)
-        _t = torch.argmax(output, dim=1)
-        # print(f'{i}-{i}-train_loss===>>{loss}')
-        matrix = confusion_matrix(pred=_pred, label=label_image)
-        pbar.set_description(f"Epoch [{i+1}/{epochs}]")
-        pbar.set_postfix(loss=loss.item())
+        accuracy = get_pixel_accuracy(pred=output, label=label_image, num_classes=2)
+        total_acc += accuracy.item() * output.size(0)
+        pbar.set_description(f"Epoch [{epoch+1}/{epochs}]")
+        pbar.set_postfix(loss=loss.item(), accuracy=accuracy.item())
+        writer.add_scalar('Loss/train', loss, epoch)
+        writer.add_scalar('Accuracy/train', accuracy, epoch)
+
+        single_output = output.cuda().data.cpu()
+        single_output = single_output[0]
+
+        single_label = label_image.cuda().data.cpu()
+        single_label = single_label[0]
+        writer.add_image('Predictions', single_output, epoch)
+        writer.add_image('Labels', torch.mul(single_label, 255).unsqueeze(0), epoch)
     print(f'train_loss = {total_loss/len(train_dl)}')
-
-
+    if epoch % 5 == 0:
+        torch.save(model.state_dict(), weight_path)
+# def train():
 #
+
+
 # if __name__ == '__main__':
 #     print(len(train_ds))
